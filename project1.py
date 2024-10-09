66b6b649b1ff022210e90f835c9c23735939c426
Afonso Ribeiro <afonsodaconceicaoribeiro@tecnico.ulisboa.pt>
2024-10-09 05:14:32 +0100

    Report first version, change .gitignore, remove comments

--- project1.py
+++ project1.py
@@ -109,11 +109,6 @@ for airport, k in cities_to_visit + [(base, K)]:
         compatible_departures = [f_departure.var for f_departure in departures
                                  if f_arrival.date.nightsBetween(f_departure.date) == k]
         wcnf.append([-f_arrival.var] + compatible_departures)
-
-    # for f_departure in departures:
-    #     compatible_arrivals = [f_arrival.var for f_arrival in arrivals
-    #                            if f_arrival.date.nightsBetween(f_departure.date) == k]
-    #     wcnf.append([-f_departure.var] + compatible_arrivals)
 ##### end: FOR EACH CITY, ARRIVAL AND DEPARTURE ARE k NIGHTS APART #####
 
 
